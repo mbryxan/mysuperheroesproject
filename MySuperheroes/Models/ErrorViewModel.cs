5154908c90b2c7a3be5c4f80611e41e56ab36a7d
Matthew Alexander <matt.bryantalex@gmail.com>
2021-02-18 06:14:20 -0800

    init

---
+++ MySuperheroes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MySuperheroes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MySuperheroes/Models/Superhero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySuperheroes.Models
+{
+    public class Superhero
+    {
+        public string  SuperheroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string  SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+    }
+}
